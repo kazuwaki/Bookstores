4f27114444088b851179883684caaeb043661095
EC2 Default User <104773179+kazuwaki@users.noreply.github.com>
2022-07-22 13:22:18 +0000

    [Add]心理テスト完成

--- app/javascript/packs/index.js
+++ app/javascript/packs/index.js
@@ -1,8 +1,17 @@
 /*global $*/
+console.log(1)
 	$(function () {
-		$(".btn").on("click", function () {
-			$(this).closest("div").css("display", "none");
-			var id = $(this).attr("href");
-			$(this[id]).addClass("fit").show("slow");
-		});
+		const buttons = document.querySelectorAll(".btn")
+		for(const button of buttons) {
+		button.addEventListener("click", function() {
+    		const currentDiv = button.closest("div");
+    		currentDiv.style.display = "none";
+
+    		const id = button.getAttribute("href");
+
+    		const nextDiv = document.querySelector(id);
+    		nextDiv.classList.add("fit");
+    		nextDiv.style.display = "block";
+		})
+		}
 	});
