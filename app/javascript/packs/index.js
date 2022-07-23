533d3ba2b3dce036cbeef41a33ea060aac23a6cb
EC2 Default User <104773179+kazuwaki@users.noreply.github.com>
2022-07-23 12:15:35 +0000

    [Add]ルーティングのバグ修正

--- app/javascript/packs/index.js
+++ app/javascript/packs/index.js
@@ -1,8 +1,8 @@
 /*global $*/
 console.log(1)
 	$(document).on ("turbolinks:load", function(){
-		const buttons = document.querySelectorAll(".btn")
-		for(const button of buttons) {
+		const buttons = document.querySelectorAll(".btn") //全てのbtnを
+		for(const button of buttons) {										//繰り返し処理
 		button.addEventListener("click", function() {
     		const currentDiv = button.closest("div");
     		currentDiv.style.display = "none";
