4eb5f489fb0cc0799c6102b6a5ca93012dbb13ed
EC2 Default User <104773179+kazuwaki@users.noreply.github.com>
2022-07-31 05:48:27 +0000

    [Add]新規投稿

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,6 +2,17 @@ class PostsController < ApplicationController
   def index
   end
 
+  def create
+    @post = Post.new(post_params)
+    @post.save
+    redirect_to request.referer
+  end
+
   def edit
+  end
+
+  private
+  def post_params
+    params.require(:post).permit(:name, :body, :type_id, :liqueur_id)
   end
 end
